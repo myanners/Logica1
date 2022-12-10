a83694df9e3d950fc786f249a82f9377711e72be
myanners <118886212+myanners@users.noreply.github.com>
2022-12-09 23:40:06 -0300

    Mostra qual é o maior entre dois números

---
+++ MaiorNumero.c
@@ -0,0 +1,30 @@
+//Programa MAIOR_NUM;
+//Var //declaração de variáveis
+//N1, N2 : Inteiro
+//Inicio
+//Escreva “1º número: “
+//Leia N1
+//Escreva “2º número: “
+//Leia N2
+//Se (N1 > N2) então
+//Escreva “O maior é o N1: “, N1
+//Senão
+//Escreva “N2 é maior ou igual a
+//N1: “, N2
+//Fim_se
+//Fim
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+int N1, N2;
+printf("1%c numero: ",167);
+scanf("%i",&N1);
+printf("2%c numero: ",167);
+scanf("%i",&N2);
+if(N1 > N2)
+printf("O maior eh o N1: %i", N1);
+else
+printf("N2 eh maior ou igual a N1: %i", N2);
+getch();
+}
