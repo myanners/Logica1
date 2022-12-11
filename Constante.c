4ad8be4adcf78522f2a0d1becb0c573992b5e495
myanners <118886212+myanners@users.noreply.github.com>
2022-12-11 00:17:03 -0300

    Declarando uma constante

---
+++ Constante.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//declarando uma constante
+
+#define nome "Meire Ellen Renna"
+
+int main()
+{
+
+printf("\n");
+printf("\n");
+printf("Imprimindo uma constante");
+printf("\n");
+printf("------------------------------------------------");
+printf("\n");
+printf("\n");
+
+printf("Nome: %s",nome);
+
+printf("\n");
+printf("\n");
+
+    return 0;
+}
