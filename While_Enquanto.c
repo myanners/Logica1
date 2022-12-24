6ab76cd26de4c3a232f000bcfe9e66b1d8648879
myanners <118886212+myanners@users.noreply.github.com>
2022-12-24 01:35:19 -0300

    Comando While teste simples de contagem

---
+++ While_Enquanto.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int contador = 1;
+
+    while (contador <= 200)
+    {
+        printf (" teste %i\n", contador);
+        ++contador;
+    }
+    return 0;
+}
