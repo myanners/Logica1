e37fbd68836306267b1f3db77ed2729271b84914
myanners <118886212+myanners@users.noreply.github.com>
2022-12-26 21:04:05 -0300

    Declarando e chamando uma função (void)

---
+++ Funcao-Aula1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    void impriaMensagem(void);
+
+    imprimaMensagem();
+
+    return 0;
+}
+
+void imprimaMensagem(void){
+
+    printf("teste");
+
+}
